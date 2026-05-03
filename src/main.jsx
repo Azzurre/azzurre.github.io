112050aeed68274bbd3f0c47c689c8123bf7a3f8
Azzurre <37148783+Azzurre@users.noreply.github.com>
2026-05-03 18:38:54 +0100

    Remove DevTerminal app wrapper imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./styles/styles.css";
-import { GameStateProvider } from "./context/GameStateContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <GameStateProvider>
-      <App />
-    </GameStateProvider>
+    <App />
   </React.StrictMode>
 );
 
-// Register service worker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(err =>
+    navigator.serviceWorker.register("/sw.js").catch((err) =>
       console.error("SW registration failed", err)
     );
   });
